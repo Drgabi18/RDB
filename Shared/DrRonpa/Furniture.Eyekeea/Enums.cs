df230d2537b5561eb73b0a79a4cb73acdd434420
Drgabi18 <2855743+Drgabi18@users.noreply.github.com>
2026-08-12 21:22:41 +0300

    Half glass of water document Types values

--- Shared/DrRonpa/Furniture.Eyekeea/Enums.cs
+++ Shared/DrRonpa/Furniture.Eyekeea/Enums.cs
@@ -5,24 +5,28 @@ namespace DanganFurniture.Enums {
 
 		NULL1 = 0,	// only appears in DR2 in maps 101, 120, 142, 143, 160
 		Person = 1,	// ID is index in the world
-		Unk1InteracteableColision = 2,	// Type is indexed object in world you click, used a lot in DR1
+		ObjectsThatAppearBasedOnFlag = 2,	// Type is indexed object in world you click, used a lot in DR1
+		ExitPointIn3D = 3,
 		Marker = 4,	// used in DR1 DEMO to stop how much you walk out if you
 					// were to walk in the trial room, no purpose in other versions
 		Bilboarded = 5,	// ID is index in the world, Unk1 seems to be type, used for lights in DR1 and palm trees in DR2
-		Unk2Interacteable = 7,	// Type is indexed object in world you click, used a lot in DR1
-		InteracteableDoors = 8,	// used a lot in DR1
+		InteracteableDoor_Unk1 = 7,	// Type is indexed object in world you click, used a lot in DR1
+		InteracteableDoors_Unk2 = 8,	// used a lot in DR1
 		Mask = 9,	// no clue, name taken from bg_100 in DR2
-		Sun = 50,	// DR2 only
-		HiddenMonokuma = 70,	// DR2 only
-		Unk71 = 71,	// DR2 only
-		Path = 90,	// DR2 Only, Unk1 represents the ID of the path, unused one in bg_025
-		NULL2 = 255	// DR2 only, padding?
+
+		// DR2 only
+		Sun = 50,
+		HiddenMonokuma = 70,
+		Unk71 = 71,
+		Path = 90,	// Unk1 represents the ID of the path, unused one in bg_025
+		NULL2 = 255,	//padding?
 
 		// Speculative
 		/*
-		SpawnPointIn3D = 3,
 		WallsAnimator? = 4,
 		ObjectWithAnimationWhenEnterirng = 6,
+		Unk10 = 10,	// only used once in DR1 in the map after celestia's trialm
+					// used in DR2 for some things
 		Color_UNK1 = 11,
 		Color_UNK2 = 17,
 		WalkInTeleport = 22,
@@ -33,7 +37,7 @@ namespace DanganFurniture.Enums {
 		WorldMesh = 80
 		Unk82 = 82,	// Uses Unk2 for something
 		
-		Unk8 = 8, Unk10 = 10, Unk12 = 12, Unk13 = 13,
+		Unk8 = 8, Unk12 = 12, Unk13 = 13,
 		Unk14 = 14, Unk16 = 16, Unk18 = 18, Unk19 = 19,
 		Unk40 = 40, Unk51 = 51, Unk52 = 52, Unk53 = 53,
 		Unk54 = 54, Unk60 = 60, Unk61 = 61, Unk62 = 62, Unk63 = 63, Unk64 = 64,
