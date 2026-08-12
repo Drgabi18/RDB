b740f479fdf98dda463bd9e8436a987047933810
Drgabi18 <2855743+Drgabi18@users.noreply.github.com>
2026-08-12 21:21:55 +0300

    Actually read the game's codes for what these file formats are, crazy I know

--- Shared/DrRonpa/Furniture.Eyekeea/Headers.cs
+++ Shared/DrRonpa/Furniture.Eyekeea/Headers.cs
@@ -1,42 +1,47 @@
 namespace DanganFurniture.Headers {
 	/*
-		TODO: As I was making these, I discovered there's more binary file fomrat
-		files in a map, in DR2 there's 0000-0003 and then the last one before .tga
-
 		==== 0000
+			INTERNAL NAME: s_bg_NNN_NN_file.dat
+			INTERNAL NAME IN DR2: s_bg_NNN_NN_file2.dat
 		first number is how many names, after that it's just offsets and then the
 		names of the .gmo models... except it can't be that because there's N
 		numbers of files and the last one always has binary data and not model data 
 
 		==== 0001
+			INTERNAL NAME: s_bg_NNN_NN_opt.dat
 		4 ints, unknown, 1 int, unknown
 
 		==== 0002
+			INTERNAL NAME: s_bg_NNN_NN_place.dat
+			INTERNAL NAME IN DR2: s_bg_NNN_NN_place2.dat
 		the code you see below, Furniture struct
 
+		=============================== DR2 ONLY ===============================
 		==== 0003
+			INTERNAL NAME IN DR2: s_bg_NNN_NN_bone_pos.dat
 		first number is how many names, after that it's just offsets and the last
 		offset is a string array ,seems to be a K=V where K is the name and
 		V is a struct, so Dictionary<string, struct>
 
 		==== last file before .tgas
-		first number is how many names, after that it's just offsets and the last
-		offset is a string array ,seems to be a K=V where K is the name and
-		V is a struct, so Dictionary<string, struct>
-
+			INTERNAL NAME: s_bg_NNN_z.col.dat
+		uses "CC DD EE FF" as a header identifier????????
+		(well, tehnically the first files after the images, remember, danganronpa
+		reads map files top to bottom, so this is the first binary file it reads)
+		is an array mesh for the walls, read at 0x0046a9e0 in the code
 	*/
 
 	public struct Room {
 		public string RoomName;	// taken from folder name
-		public List<string> ModelNames;	// file 0000
-		public UnkStruct1 Unk1;	// file 0001
-		public List<Furniture> Objects;	// file 0002
+		public List<string> ModelNameFile;	// file 0000
+		public OptionsFile Options;	// file 0001
+		public List<Furniture> Places;	// file 0002
 		public Dictionary<string, AABBStruct> AABB;	// file 0003
 		//public Dictionary<string, UnkStruct3> Masks; // last file before iamges
 	}
 
 	// 0001
-	public struct UnkStruct1 {
+	public struct OptionsFile {
 		public int Unk1;
 		public int Unk2;
 		public int Unk3;
@@ -88,5 +93,15 @@ namespace DanganFurniture.Headers {
 			//TopLeftCorner = new float[3];
 			//BottomRightCorner = new float[3];
 		}
+	}
+
+	// colision file
+	public struct Colision {
+		uint Identifier = 0xCCDDEEFF;
+		int FileSize;	// with identifier included 
+		int Unk2;	// header size?
+		int SizeOfObjects;	// size of the list at the top after the identifier
+
+		public Colision() {}
 	}
 }
