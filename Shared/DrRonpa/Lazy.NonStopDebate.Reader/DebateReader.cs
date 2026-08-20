b2da0fc5abc2ddab533b886e9ac9a21334db5553
Drgabi18 <2855743+Drgabi18@users.noreply.github.com>
2026-08-20 21:47:23 +0300

    Rework the debate reader a bit

--- Shared/DrRonpa/Lazy.NonStopDebate.Reader/DebateReader.cs
+++ Shared/DrRonpa/Lazy.NonStopDebate.Reader/DebateReader.cs
@@ -1,36 +1,11 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text.Json;
 
 class Program {
-
-	// unused and here just for reference
-	[StructLayout(LayoutKind.Sequential)]
-	struct CharDebateInfo {
-		short Index; // 0-indexed
-		short Unk1, Unk2, Unk3, Unk4, Unk5, Unk6, Unk7, Unk8, Unk9, Unk10, Unk11;
-		short Unk12, Unk13, Unk14, Unk15, Unk16, Unk17, Unk18, Unk19;
-		short CharacterID; // 0-Hajime, 1-Nagito etc.
-		short ExpressionID;
-		short Unk22, Unk23, Unk24, Unk25, Unk26, Unk27, Unk28, Unk29;
-		short Unk30, Unk31, Unk32, Unk33; // for some reason DR2 includes these 4 extra shorts
-	} // 60-bytes in DR1, 68-bytes in DR2
-
-	/*
-	[StructLayout(LayoutKind.Sequential)]
-	struct DebateInfo {
-		short Duration;
-		short HowManyEvents;
-		CharDebateInfo[] Debates;
-	} // this should have been a class, i could easily do `CharDebateInfo[HowManyEvents] Debates` but alas
-	*/
-
 	static void Main(string[] args) {
-
 		// i gave up on making a switch to make this easier
 		string DebateFormat = args[0]; // nonstop, kokoro, hanron, scrum
-		string GameID = args[1]; // "DR1", "DR2" or "DR3"
+		string GameID = args[1]; // "DR1", "DR2", "DRV3" or "Special"
 		int PrintMode = Convert.ToInt16(args[2]); // print mode
 		string FileOrFolderPath = args[3]; // has to be the folder with the .dat files
 		
@@ -39,7 +14,8 @@ class Program {
 			default:
 			case "DR1"	: SizeOfCharDebate = 60; break;
 			case "DR2"	: SizeOfCharDebate = 68; break;
-			case "DRV3"	: SizeOfCharDebate = 105; break;
+			case "DRV3"	: SizeOfCharDebate = 105; break;	// unconfirmed, stolen from other attempts at the same thing
+			case "Special": SizeOfCharDebate = 51; break;	// nonstop_90_007.dat in DR1-2 on Vita
 		}
 
 		List<string> FileNamesForAlphabet = new();
@@ -60,23 +36,21 @@ class Program {
 		FileNamesForAlphabet.Sort();
 		
 		foreach (string file in FileNamesForAlphabet) {
-			short Duration;
-			short HowManyEvents;
 
 			using (FileStream fs = File.Open(file, FileMode.Open)) {
 			using (BinaryReader br = new(fs) ) {
-				// and we're commenting ot this code cause i don't know 
-				// how to cast a byte array to a struct
-				// 19/07/2026: ok i found out how to cast byte array to struct, check EV8.Parser
-				/*
+				
+				// originally there was an attempt at marshalling a byte array to
+				// a struct here, but that was removed, you can see however a working
+				// implementation of it in EV8.Parser as of 20 august 2026
+
+				short Duration;
+				short HowManyEvents;
 				Duration = br.ReadInt16();
 				HowManyEvents = br.ReadInt16();
-				for (int i = 0; i < HowManyEvents; i++) {
-					br.Read(Marshal.SizeOf(CharDebateInfo));
-				}
-				*/
+
 				switch (PrintMode) {
-					case 0:
+					case 0:		// equivalent to a memory viewer set to interpret integers
 						Console.WriteLine(file);
 						int _indx = 0;
 						while (br.BaseStream.Position < br.BaseStream.Length) {
@@ -87,10 +61,8 @@ class Program {
 						}
 						Console.WriteLine();
 						break;
-					case 1:
+					case 1:		// curiosity
 						Console.WriteLine(file);
-						Duration = br.ReadInt16();
-						HowManyEvents = br.ReadInt16();
 						short[] GodKillMe = new short[(SizeOfCharDebate/2)];
 						for (int i = 0; i < HowManyEvents; i++) {
 							for (int j =0; j < (SizeOfCharDebate/2); j++) {
@@ -102,12 +74,34 @@ class Program {
 							Console.WriteLine("Expression is {0:D}", GodKillMe[22]);
 						}
 						break;
-					case 2:
-						Duration = br.ReadInt16();
-						HowManyEvents = br.ReadInt16();
+					case 2:		// CSV format for easy parsing
+						// very ugly hack, only assumes nonstop, breaks on other styles
+						List<string> CSVHeaderString = new List<string>
+							{
+							"FileName", "IndexInFile", "HowManyTimesShootPinkText",
+							"Unk3",	// SpiralFramework and bitesized already documented
+									// these unknown values, do i copy it or find myself
+									// what they mean?
+							"TruthBulletID", "ConsentBulletID",
+							"Unk6",	// ditto
+							"HasValidShootSpot",
+							"Unk88", "Unk9",  "Unk10",  "Unk11",  "Unk12",	// ditto
+							"TextX", "TextY", "TextMoveSpeed", "DirectionTextIsGoing",
+							"Unk17",  "Unk18",  "Unk19",	// ditto
+							"RotationAngle", "ValueToAddToAngle", "CharacterID",
+							"Expression", "CameraID", "IconShake",
+							"Unk26", "Unk27",  "Unk28",  "Unk29",	// ditto
+							"StandToPointAt",
+							};
+
+							// DR2 includes 4 extra ints at the end for the sword minigame
+							if (GameID == "DR2"){
+								CSVHeaderString.AddRange( "Unk31", "Unk32", "Unk33", "Unk34");
+							}
+
+						Console.WriteLine(string.Join(";",CSVHeaderString));
 						for (int i = 0; i < HowManyEvents; i++) {
 							Console.Write(Path.GetFileName(file));
-							Console.Write("\t");
 							for (int j =0; j < (SizeOfCharDebate/2); j++) {
 								Console.Write(";{0}",br.ReadInt16());
 							}
