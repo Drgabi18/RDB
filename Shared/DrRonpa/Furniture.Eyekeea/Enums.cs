9a35360aab76b0f576b3818d4e7af761f076d2f1
Drgabi18 <2855743+Drgabi18@users.noreply.github.com>
2026-08-21 22:21:36 +0300

    Document furniture object tpyes better after checking the game's code (shocker, I know)

--- Shared/DrRonpa/Furniture.Eyekeea/Enums.cs
+++ Shared/DrRonpa/Furniture.Eyekeea/Enums.cs
@@ -1,83 +1,89 @@
 namespace DanganFurniture.Enums {
 	public enum PrintModesEnum : int { JsonSerialized, LazyGodot }
 	
+	// TODO: Would these work better as objects? For example, Type 1 is people
+	// in the world and uses the Positions as position in the world, but Type 40
+	// which sets the overlay color casts (char)(int)(float) to a byte array to
+	// transform that into a value, should we show that or just leave a comment?
 	public enum FurnitureTypes : int {
-
-		NULL1 = 0,	// only appears in DR2 in maps 101, 120, 142, 143, 160
-		Person = 1,	// ID is index in the world
+		Person = 1,	// ID is index in the world, Size[1] is unused
 		ObjectsThatAppearBasedOnFlag = 2,	// Type is indexed object in world you click, used a lot in DR1
 		ExitPointIn3D = 3,	// unused exists for toilets in bg_252
-		Marker = 4,	// used in DR1 DEMO to stop how much you walk out if you
-					// were to walk in the trial room, no purpose in other versions
+		Marker = 4,	// used in DR1 DEMO to change those unknown -5000 to 5000 values
+					// potentially limit how much you cal walk if you had the posibility
+					// to walk in the trial room, no purpose in other versions
+					// still present only in trial maps even in DR2
 		Bilboarded = 5,	// ID is index in the world, Unk1 seems to be type, used
 						// for lights in DR1 and palm trees in DR2
-		InteracteableDoor_Unk1 = 7,	// Type is indexed object in world you click,
-									// used a lot in DR1, has a different
-									// purpose in DR2 where they hold the string
-		InteracteableDoors_Unk2 = 8,	// used a lot in DR1, has a different
-										// purpose in DR2 where they hold the string
-		Mask = 9,	// no clue, name taken from bg_100 in DR2
+		DR1_UNK_DR2CameraLimit = 6,	// limit how much you can look around,
+									// Pos[0] DR_X_CameraCenter, Pos[1] DR_Y_CameraCenter, Pos[2] DR_Z_CameraCenter
+									// Size[0] is DR_DistanceFromCenterPoint_RoomMode, UNK2 IS FLOAT HERE, Size[1] unused
+		Interacteable_UNK1 = 7,	// Type is indexed object in world you click,
+								// used a lot in DR1, has a different
+								// purpose in DR2 where they hold the string
+		Interacteable_Unk2 = 8,	// used a lot in DR1, has a different
+								// purpose in DR2 where they hold the string
+		DR1_UNK_DR2Mask = 9,	// no clue, name taken from bg_100 in DR2
 
-		// DR2 only
-		WalkInTeleport = 22,	// only used once at the end of the chapter 6 corridor, could just be the sun
-		Sun = 50,
+		DR1_UNK_DR2SetBloom = 10,	// only used once in DR1 in the map after celestia's trial
+					// used in DR2 for some things
+		
+		// ============================= DR2 only =============================
+		STOP = 0,	// used in DR2 to stop reading the furniture early
+		UNK_SetColors = 11,	// seems to change the world only and not the skybox
+		UNK_Lighting = 12,	// sets some stuff for the unused lighting system
+		UNK_Background = 16,	// only present in maps with skyboxes that follow you
+		WalkInTeleport = 22,	// only used once at the end of the chapter 6 corridor
+								// sees if player is behind it (on all axis), after it gets ID,
+								// it searches -20 bytes in memory behind itself?????
+		Fog = 30,	// position is color, size[1] is distance
+		OverlayTop = 40,	// overlay top color, Unk1 is Overlay type
+		OverlayBottom = 41,	// overlay bottom color
+		Sun = 50,	// object read in the load map code
+		UNK_52 = 52,	// object read in the load map code,
+						// get read next to overlay colors
+						// only appears to have values in bg_266 (makoto cihiro room) where the wireframe begins
+		UNK_53 = 53,	// object read in the load map code,
+						// get read next to overlay colors
+						// only used in bg_266 (makoto cihiro room)
+		UNK_54 = 54,	// object read in the load map code,
+						// supposed to change lens flare type, is broken, makes sun visible through absolutley everything
+		UNK_60 = 60,	// object read in the load map code
 		WarpAroundPointIn2D = 61,
 		HowMuchToMoveInTwilight2D = 62,	// Size[0] is X pos, Size[1] is how long, needs to be rotated on the Y axis by 90deg
 		HowMuchToMoveInTwilight2D_2 = 63,	// roughly the same thing?
-		
-		HiddenMonokuma = 70,	// by name, mostly 0 exceept a few
+		UNK_64 = 64,	// only used once in bg_905, not related to the crash
+		HiddenMonokuma = 70,	// object name contains monokuma, may set the id in the world for him
+		UNK_72 = 72,	// ???? get model to render when climbing up floors, may set which objects are visible in multi floor maps
+		UNK_73 = 73,	// related to camerea zooming to object to talk
+		UNK_75 = 75,	// ???? could be related to the camera when you first visit the hotel and after the start of investigation in chapter 1
+		ChangeFov = 76,	// change fov, code at 0x0056cc84, Pos[0] is FOV
+		LockPerspectiveHorizontally = 78,	// lock perspective to only look left and right
+		LockPerspectiveVertically = 79,	// gee i wonder which map uses this
 		PathForCamera = 80,	//eg bg_002, airport when you start pans front to back
-		AmbientColor = 84,	// not overlay, mixes the colors from ID 1 with 2
-							// and the resulting middle is the color, maybe
-							// Size is used for how much
-		Path = 90,	// Unk1 represents the ID of the path, unused one in bg_025
-		NULL2 = 255,	//padding?
-
-		// Speculative
+		CameraModeChange = 83,	//camera mode change in strawberry and grape house when you inspect the park and lounge
+		DissapearingBlockEffects = 84,
+		PathWhenWalingInRoomsWithFloors = 90,	// Unk1 represents the ID of the path, unused one in bg_025
+		SANITY_CHECK = 255,	// DR2 starts reading the furniture only if this object exists
 
 		/*
-		WallsAnimator? = 4,
-		Unk6 = 6,	// maybe objects with animations, in DR2 it roughly matches
-					// stuff like in the SAW room and Strawberry where your camera
-					// changes for those scenes
-		Unk10 = 10,	// only used once in DR1 in the map after celestia's trial
-					// used in DR2 for some things
-		
-		// DR2 only
-		Color_UNK11 = 11,
-		
-		Unk13 = 13,	// only uses ID, Rotation and Unk2
-		Unk16 = 16,	// only uses ID and Unk1
-		
-		Color_UNK17 = 17,	// color transition when entering room?
-		Color_UNK18 = 18,
-		
-		Unk19 = 19,	// position[x] is 0.5, ID is used for something
-					// used in saw room and java military things
-		Unk30 = 30,	// may be animations and how things move, 255 is -1, Size is
-					// how position[y] and [z] change
 
-		TopOverlayColor = 40,	// matches in bg_096 but not bg_906, see if 41 is Bottom Overlay Color or Camera Pos
-		BottomOverlayColor = 41,
+		// ========== Speculative ==========
+		// TODO: I deleted some of the UNK from here without documenting, re-add them back later
+		UNK_11 = 11,
+		UNK_13 = 13,	// only uses ID, Rotation and Unk2		
+		UNK_14 = 14, 
+		UNK_19 = 19,	// position[x] is 0.5, ID is used for something
+						// used in saw room and java military things
+		UNK_51 = 51,	// uses ID and Unk1 for something, Unk1 may be Model Index
+		UNK_66 = 66,	// uses ID and Pos[x] for something
+		UNK_67 = 67,
+		UNK_77 = 77,
+		UNK_81 = 81,
+		UNK_82 = 82,	// Uses Unk2 for something
+		UNK_85 = 85,
+		UNK_86 = 86,
 
-		CameraPos = 41,	// position sits roughly where the camera is in DR2's
-						// bg_169, maybe 51 is what it points at?
-		
-		Unk51 = 51,	// uses ID and Unk1 for something, Unk1 may be Model Index
-		Unk52 = 52,	// only appears to have values in bg_266 (makoto cihiro room) where the wireframe begins
-		Unk53 = 53,	// only used in bg_266 (makoto cihiro room)
-		Unk62 = 62,	// only appears in the Twilight Murder Mystery rooms
-		Unk64 = 64,	// only used once in bg_905, is this why it crashed?
-		Unk66 = 66,	// uses ID and Pos[x] for something
-		Unk71 = 71,	// roughly in the position of hidden monokumas
-		
-		Unk82 = 82,	// Uses Unk2 for something
-		
-		Unk12 = 12, Unk14 = 14,  
-		Unk54 = 54, Unk60 = 60, Unk61 = 61, Unk63 = 63,
-		Unk67 = 67, Unk72 = 72, Unk73 = 73, Unk75 = 75,
-		Unk76 = 76, Unk77 = 77, Unk78 = 78, Unk79 = 79, Unk81 = 81,
-		Unk83 = 83, Unk85 = 85, Unk86 = 86,
 		*/
 	}
 }
