d1ffb8adba2d670d20968f5c139402841d0a21f0
Drgabi18 <2855743+Drgabi18@users.noreply.github.com>
2026-08-31 19:40:43 +0300

    GodotSharp exists, so no more need for wishful thinking

--- Shared/DrRonpa/Furniture.Eyekeea/PrintModes.cs
+++ Shared/DrRonpa/Furniture.Eyekeea/PrintModes.cs
@@ -1,4 +1,3 @@
-// TODO: using Godot;	// find a way to add the #if GODOT preprocessor in the csproj
 using System.Text.Json;
 using DanganFurniture.Enums;
 using DanganFurniture.Headers;
@@ -29,7 +28,16 @@ namespace DanganFurniture.PrintModesClass {
 				} else {
 					NodeName = String.Concat(Enum.GetName(typeof(FurnitureTypes), Object.Type), "_Node_", Indexer);
 				}
-				// TODO: Godot.Transform3D WHY DID THEY HAVE TO MAKE A CLASS NOT SUPPORTED HERE
+				
+				Godot.Basis Test1 = new Godot.Basis().Rotated(new Godot.Vector3(Object.Position[0], Object.Position[1], Object.Position[2]),
+					(float)Double.DegreesToRadians(Object.Rotation));
+				Godot.Transform3D Test2 = new Godot.Transform3D(Test1, new Godot.Vector3(0, 0, 0));
+
+				Console.WriteLine(Test1);
+				Console.WriteLine(Test2);
+				
+				Console.ReadKey();
+
 				Console.WriteLine("[node name=\"{0}\" type=\"Marker3D\" parent=\"{1}\" unique_id={2}]",
 					NodeName, Map.RoomName, Indexer * 100);
 				Console.WriteLine("transform = Transform3D({0}, 0, 0, 0, {1}, 0, 0, 0, {2}, {3}, {4}, {5})",
@@ -50,10 +58,6 @@ namespace DanganFurniture.PrintModesClass {
 				Console.WriteLine("pixel_size = 0.5");
 				Console.WriteLine("billboard = 2");
 				Console.WriteLine("texture = ExtResource(\"{0}\")", GodotSvgIdentifierWhatever);
-				// ALTERNATIVE creating a box because it's impossible to see these on a 4K monitor
-				//Console.WriteLine("[node name=\"CSGBox3D\" type=\"CSGBox3D\" parent=\"{0}\" unique_id={1}]",
-				// 	NodeName, Randomy.Next());
-				//Console.WriteLine("size = Vector3(100, 100, 100)");
 				Console.WriteLine();
 			}
 
